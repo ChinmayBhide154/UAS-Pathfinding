e78769147cc931b52ce9f37ce0dd67e06ae81874
Chinmay Bhide <Chinmay.Bhide154@gmail.com>
2022-10-15 11:34:04 -0700

    completed distanceFrom function. Testing still required

--- UAS-Pathfinding/Waypoint.cpp
+++ UAS-Pathfinding/Waypoint.cpp
@@ -1,1 +1,6 @@
 #include "Waypoint.h"
+#include <cmath>
+double Waypoint::distanceFrom(Waypoint* target) {
+	return sqrt(pow(target->x - this->x, 2) + pow(target->y - this->y, 2));
+}
+
--- UAS-Pathfinding/Waypoint.h
+++ UAS-Pathfinding/Waypoint.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Waypoint
 {
+	//Properties
+	public:
+		double x;
+		double y;
+		double lat;
+		double lon;
+		string name;
+
+	//Constructor
+
+	Waypoint(double xLat, double yLong, bool isGps, string name);
+
+	//Functions
+
+	double distanceFrom(Waypoint* target);
 };
 
