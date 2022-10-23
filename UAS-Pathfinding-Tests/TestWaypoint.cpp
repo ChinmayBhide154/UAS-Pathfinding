ad7953b78cc8aea02856fc30feb2b57e4df82942
Chinmay Bhide <Chinmay.Bhide154@gmail.com>
2022-10-23 16:30:37 -0700

    Adding more tests to verify obstruction detection. Still need more testing for isObstructionN method

--- UAS-Pathfinding-Tests/TestWaypoint.cpp
+++ UAS-Pathfinding-Tests/TestWaypoint.cpp
@@ -5,6 +5,11 @@
 Waypoint waypointA;
 Waypoint waypointB;
 
+Waypoint waypoint1;
+Waypoint waypoint2;
+Waypoint waypoint3;
+Waypoint waypoint4;
+
 TEST(WaypointTests, projectConfigTest) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
@@ -66,6 +71,110 @@ TEST(WaypointTests, distanceFrom3B) {
 	waypointB.y = 68.456;
 
 	ASSERT_EQ(206, int(waypointB.distanceFrom(&waypointA)));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment1) {
+	waypoint1.x = 0; waypoint1.y = 0;
+	waypoint2.x = 0; waypoint2.y = 0;
+	waypoint3.x = 0; waypoint3.y = 0;
+
+	ASSERT_EQ(true, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment2) {
+	waypoint1.x = 0; waypoint1.y = 0;
+	waypoint2.x = 1; waypoint2.y = 1;
+	waypoint3.x = 2; waypoint3.y = 2;
+
+	ASSERT_EQ(true, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment3) {
+	waypoint1.x = 0; waypoint1.y = 0;
+	waypoint2.x = 1; waypoint2.y = 1;
+	waypoint3.x = 5; waypoint3.y = 5;
+
+	ASSERT_EQ(true, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment4) {
+	waypoint1.x = 0; waypoint1.y = 0;
+	waypoint2.x = 100; waypoint2.y = 100;
+	waypoint3.x = 2; waypoint3.y = 3;
+
+	ASSERT_EQ(false, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment5) {
+	waypoint1.x = 100; waypoint1.y = 100;
+	waypoint2.x = 100; waypoint2.y = 100;
+	waypoint3.x = 2; waypoint3.y = 3;
+
+	ASSERT_EQ(true, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, onWaypointLineSegment6) {
+	waypoint1.x = -1; waypoint1.y = -1;
+	waypoint2.x = 0; waypoint2.y = 0;
+	waypoint3.x = 5; waypoint3.y = 1;
+
+	ASSERT_EQ(false, waypoint2.onWaypointLineSegment(&waypoint1, &waypoint2, &waypoint3));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, CheckObstructions1) {
+	waypoint1.x = -1; waypoint1.y = 0;
+	waypoint2.x = 1; waypoint2.y = 0;
+	waypoint3.x = 0; waypoint3.y = -1;
+	waypoint4.x = 0; waypoint4.y = 1;
+
+	EXPECT_EQ(true, waypoint3.isObstruction2(&waypoint1, &waypoint2, &waypoint4));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, CheckObstructions2) {
+	waypoint1.x = -1; waypoint1.y = 0;
+	waypoint2.x = 1; waypoint2.y = 0;
+	waypoint3.x = 1; waypoint3.y = -1;
+	waypoint4.x = 2; waypoint4.y = 1;
+
+	EXPECT_EQ(false, waypoint3.isObstruction2(&waypoint1, &waypoint2, &waypoint4));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, CheckObstructions3) {
+	waypoint1.x = 0; waypoint1.y = 0;
+	waypoint2.x = 5; waypoint2.y = 0;
+	waypoint3.x = -5; waypoint3.y = 0;
+	waypoint4.x = -1; waypoint4.y = 0;
+
+	EXPECT_EQ(false, waypoint3.isObstruction2(&waypoint1, &waypoint2, &waypoint4));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, CheckObstructions4) {
+	waypoint1.x = -1; waypoint1.y = -1;
+	waypoint2.x = 1; waypoint2.y = 1;
+	waypoint3.x = 2; waypoint3.y = 2;
+	waypoint4.x = 3; waypoint4.y = 3;
+
+	EXPECT_EQ(false, waypoint3.isObstruction2(&waypoint1, &waypoint2, &waypoint4));
+	EXPECT_TRUE(true);
+}
+
+TEST(WaypointTests, CheckObstructions5) {
+	waypoint1.x = 5.2; waypoint1.y = 6.2;
+	waypoint2.x = 9.56; waypoint2.y = 10.67;
+	waypoint3.x = 5.2; waypoint3.y = 7;
+	waypoint4.x = 8; waypoint4.y = -1;
+
+	EXPECT_EQ(true, waypoint3.isObstruction2(&waypoint1, &waypoint2, &waypoint4));
 	EXPECT_TRUE(true);
 }
 
--- UAS-Pathfinding/Waypoint.h
+++ UAS-Pathfinding/Waypoint.h
@@ -15,6 +15,14 @@ class Waypoint
 		double lon;
 		string name;
 
+
+	//Enums
+	enum Orientation{
+		Collinear = 0,
+		Clockwise = 1,
+		CounterClockwise = 2
+	};
+
 	//Constructors
 	Waypoint();
 	Waypoint(double xLat, double yLong, bool isGps, string name, double refLat, double refLong);
@@ -23,10 +31,9 @@ class Waypoint
 	double distanceFrom(Waypoint* target);
 	bool isObstructionN(vector<Waypoint*> waypoints, Waypoint* target);
 	
-private:
 	bool isObstruction2(Waypoint* p1, Waypoint* p2, Waypoint* target);
 	bool onWaypointLineSegment(Waypoint* waypoint1, Waypoint* waypoint2, Waypoint* waypoint3);
 	int findOrientation(Waypoint* waypoint1, Waypoint* waypoint2, Waypoint* waypoint3);
-	bool doIntersect(Waypoint* target, Waypoint* waypoint1, Waypoint* waypoint2);
+	//bool doIntersect(Waypoint* target, Waypoint* waypoint1, Waypoint* waypoint2);
 };
 
