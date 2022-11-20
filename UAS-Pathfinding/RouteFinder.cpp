78b1edaf01fd517bcb6b2bf3614195a7fb22d8d9
Justin Wong <vsajustinwong@gmail.com>
2022-11-19 16:13:18 -0800

    revert to non-poly time

--- UAS-Pathfinding/RouteFinder.cpp
+++ UAS-Pathfinding/RouteFinder.cpp
@@ -14,10 +14,11 @@
 #define UINT32_MAX  		4294967295
 #define INT_DOUBLE_ACCURACY 1E12
 
-RouteFinder::RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint)
-	: routes(routes) {
+RouteFinder::RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint, double maxDist)
+	: routes(routes), maxDist(maxDist) {
 		// Find route closest to the first point, use that as first node of traversal
 		double distToStartingRoute = FLOAT_MAX;
+		this->startingPoint = firstPoint;
 		uint32_t firstRouteIndex = 0;
 		// n-time
 		for (uint32_t i = 0; i < this->routes.size(); i++) {
@@ -27,6 +28,7 @@ RouteFinder::RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint)
 				firstRouteIndex = i;
 			}
 		}
+		this->distToStartingRoute = distToStartingRoute;
 
 		// Set first route to the first element of routes vector
 		this->routes.insert(this->routes.begin(), this->routes[firstRouteIndex]);
@@ -44,8 +46,11 @@ RouteFinder::RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint)
 					Waypoint* endOfFirst = routes[i]->waypoints[routes[i]->waypoints.size() - 1];
 					Waypoint* startOfNext = routes[j]->waypoints[0];
 
+					double distance = endOfFirst->distanceFrom(startOfNext);
+					if (distance >= this->maxDist) adjMatrix[i][j] = -1;
+
 					// Edge weight = total_dist / $ => smaller is better
-					adjMatrix[i][j] = (endOfFirst->distanceFrom(startOfNext) + routes[j]->getDistance()) / routes[j]->dollarValue;
+					adjMatrix[i][j] = (distance + routes[j]->getDistance()) / routes[j]->dollarValue;
 					_ASSERT(adjMatrix[i][j] >= 0);
 				}
 			}
@@ -63,94 +68,163 @@ RouteFinder::~RouteFinder() {
 * std::vector<Route*> RouteFinder::findShortestTraversal()
 *	- Returns: the optimal sequence of routes to take
 */
-std::vector<Route*> RouteFinder::findShortestTraversal(double maxDist) {
+std::vector<Route*> RouteFinder::findShortestTraversal() {
 	std::vector<Route*> retArr = std::vector<Route*>();
 
-	// Edges: ranks
-	// Vertices: [starting loc], and after that this->routes
-	// https://en.wikipedia.org/wiki/Christofides_algorithm
 
-	// Initialize helper node lists and adjacency matrices
-	bool* mstNodes = new bool[this->numNodes];
-	bool* mstOddDegreeNodes = new bool[this->numNodes];
-	memset(mstNodes, false, sizeof(bool) * this->numNodes);
-	memset(mstOddDegreeNodes, false, sizeof(bool) * this->numNodes);
+	bool* visited = new bool[this->numNodes];
+	memset(visited, false, sizeof(bool) * this->numNodes);
+	double cost = 0;
+	minimumCost(0, visited, &cost, &retArr);
+	retArr.pop_back();
 
-	double** mstEdges = new double*[this->numNodes];
-	double** subgraphO = new double*[this->numNodes];
-	double** minWeightMatch = new double*[this->numNodes];
 
-	for (uint32_t i = 0; i < this->numNodes; i++) {
-		mstEdges[i] = new double[this->numNodes];
-		subgraphO[i] = new double[this->numNodes];
-		minWeightMatch[i] = new double[this->numNodes];
+
+	// // Edges: ranks
+	// // Vertices: [starting loc], and after that this->routes
+	// // https://en.wikipedia.org/wiki/Christofides_algorithm
+	//
+	//
+	// // Initialize helper node lists and adjacency matrices
+	// bool* mstNodes = new bool[this->numNodes];
+	// bool* mstOddDegreeNodes = new bool[this->numNodes];
+	// memset(mstNodes, false, sizeof(bool) * this->numNodes);
+	// memset(mstOddDegreeNodes, false, sizeof(bool) * this->numNodes);
+
+	// double** mstEdges = new double*[this->numNodes];
+	// double** subgraphO = new double*[this->numNodes];
+	// double** minWeightMatch = new double*[this->numNodes];
+
+	// for (uint32_t i = 0; i < this->numNodes; i++) {
+	// 	mstEdges[i] = new double[this->numNodes];
+	// 	subgraphO[i] = new double[this->numNodes];
+	// 	minWeightMatch[i] = new double[this->numNodes];
 		
-		// Init values to -1
-		memset(mstEdges[i], -1, sizeof(double) * this->numNodes);
-		memset(subgraphO[i], -1, sizeof(double) * this->numNodes);
-		memset(minWeightMatch[i], -1, sizeof(double) * this->numNodes);
-	}
+	// 	for (uint32_t j = 0; j < this->numNodes; j++) {
+	// 		mstEdges[i][j] = -1;
+	// 		subgraphO[i][j] = -1;
+	// 		minWeightMatch[i][j] = -1;
+	// 	}
+	// }
 
-	// Find MST via Prim's
-	primsMST(mstEdges, mstNodes);
+	// // Find MST via Prim's
+	// primsMST(mstEdges, mstNodes);
 
-	// Find set O of nodes with odd degree in MST. This set should have an even number of nodes.
-	getOddDegreeNodes(mstEdges, mstNodes, mstOddDegreeNodes);
+	// // Find set O of nodes with odd degree in MST. This set should have an even number of nodes.
+	// getOddDegreeNodes(mstEdges, mstNodes, mstOddDegreeNodes);
 
-	// Find min-weight perfect matching M from induced subgraph by nodes from O
-	uint32_t subGraphONumNodes, subGraphONumEdges;
-	extractSubgraph(&subGraphONumNodes, &subGraphONumEdges, mstOddDegreeNodes, subgraphO);
-	_ASSERT(subGraphONumNodes % 2 == 0);		// Enforce even number of nodes
-	findMinWeightPerfectMatching(&subGraphONumNodes, &subGraphONumEdges, mstOddDegreeNodes, subgraphO, minWeightMatch);
+	// // Find min-weight perfect matching M from induced subgraph by nodes from O
+	// uint32_t subGraphONumNodes, subGraphONumEdges;
+	// extractSubgraph(&subGraphONumEdges, &subGraphONumNodes, mstOddDegreeNodes, subgraphO);
+	// _ASSERT(subGraphONumNodes % 2 == 0);		// Enforce even number of nodes
+	// findMinWeightPerfectMatching(&subGraphONumNodes, &subGraphONumEdges, mstOddDegreeNodes, subgraphO, minWeightMatch);
 	
-	// Combine edges of of mst and minWeightMatch to form connected multigraph (each node should have an even number degree)
-	// DEBUG: Not sure if MST and MinWeightMatch has mutually exlusive edges
-	for (uint32_t i = 0; i < this->numNodes; i++) {
-		for (uint32_t j = 0; j < this->numNodes; j++) {
-			if (i == j) {
-				_ASSERT(mstEdges[i][j] == -1);
-				_ASSERT(minWeightMatch[i][j] == -1);
-			}
-			else if (minWeightMatch[i][j] != -1) {
-				_ASSERT(mstEdges[i][j] == -1);		// DEBUG: Remove this if necessary
-				mstEdges[i][j] = minWeightMatch[i][j];
-			}
-			else if (mstEdges[i][j] != -1) {
-				_ASSERT(minWeightMatch[i][j] == -1);
-			}
-		}
-	}
+	// // Combine edges of of mst and minWeightMatch to form connected multigraph (each node should have an even number degree)
+	// // DEBUG: Not sure if MST and MinWeightMatch has mutually exlusive edges
+	// for (uint32_t i = 0; i < this->numNodes; i++) {
+	// 	for (uint32_t j = 0; j < this->numNodes; j++) {
+	// 		if (i == j) {
+	// 			_ASSERT(mstEdges[i][j] == -1);
+	// 			_ASSERT(minWeightMatch[i][j] == -1);
+	// 		}
+	// 		else if (minWeightMatch[i][j] != -1) {
+	// 			_ASSERT(mstEdges[i][j] == -1);
+	// 			mstEdges[i][j] = minWeightMatch[i][j];
+	// 		}
+	// 		else if (mstEdges[i][j] != -1) {
+	// 			_ASSERT(minWeightMatch[i][j] == -1);
+	// 		}
+	// 	}
+	// }
 	
-	// Form Eulerian Circuit, without repeating nodes
-	vector<Route*>* eulerTour = new vector<Route*>();
-	findEulerTour(mstNodes, mstEdges, eulerTour);		// mstEdges unsuable after this
+	// // Form Eulerian Circuit, without repeating nodes
+	// vector<Route*>* eulerTour = new vector<Route*>();
+	// findEulerTour(mstNodes, mstEdges, eulerTour);		// mstEdges unsuable after this
 
 	// TODO: Get subset of routes that won't exceed travel distance, reject if < 50% routes traversed
 	// DEBUG: Must include the "first" Route stored in this->routes AND account for time to return to it at the end!
-	
 
-	// TODO: Get route with greatest value out of the subset
+	// // Get working first, first subset probably best
+	// double distance = this->distToStartingRoute;
+	// uint32_t iter = 0;
+	// while (distance < this->maxDist && iter < eulerTour->size()) {
+	// 	// Check if we can return to start point from this
+	// 	double distFromPrevRoute = 0;
+	// 	double distBackToStart = (*eulerTour)[iter]->waypoints[(*eulerTour)[iter]->waypoints.size() - 1]->distanceFrom(this->startingPoint);
+	// 	if (retArr.size() != 0) {
+	// 		distFromPrevRoute = retArr[retArr.size() - 1]->waypoints[retArr[retArr.size() - 1]->waypoints.size() - 1]->distanceFrom((*eulerTour)[iter]->waypoints[0]);
+	// 	}
+	// 	if (distFromPrevRoute + distance + distBackToStart > this->maxDist) {
+	// 		break;
+	// 	}
+	// 	distance += distFromPrevRoute + (*eulerTour)[iter]->getDistance();
+	// 	retArr.push_back((*eulerTour)[iter]);
+	// 	iter++;
+	// }
+
+	// // Stretch TODO: Get route with greatest value out of the subset
 
 
-	// Delete created vectors in heap. only deletes the vectors, not the edges/nodes in it
-	for (uint32_t i = 0; i < this->numNodes; i++) {
-		delete[] mstEdges[i];
-		delete[] subgraphO[i];
-		delete[] minWeightMatch[i];
-	}
-	delete[] mstEdges;
-	delete[] subgraphO;
-	delete[] minWeightMatch;
+	// // Delete created vectors in heap. only deletes the vectors, not the edges/nodes in it
+	// for (uint32_t i = 0; i < this->numNodes; i++) {
+	// 	delete[] mstEdges[i];
+	// 	delete[] subgraphO[i];
+	// 	delete[] minWeightMatch[i];
+	// }
+	// delete[] mstEdges;
+	// delete[] subgraphO;
+	// delete[] minWeightMatch;
 
-	delete[] mstNodes;
-	delete[] mstOddDegreeNodes;
+	// delete[] mstNodes;
+	// delete[] mstOddDegreeNodes;
 
-	delete eulerTour;
+	// delete eulerTour;
 	
 	return retArr;
 }
 
+// Dynamic Programming TSP (non-poly time) Helper
+void RouteFinder::minimumCost(uint32_t routeIndex, bool* visited, double* cost, std::vector<Route*>* retArr) {
+	uint32_t nIndex;
+	visited[routeIndex] = true;
+	retArr->push_back(this->routes[routeIndex]);
+	nIndex = closest(routeIndex, visited, cost);
+
+	if (nIndex == UINT32_MAX) {
+		nIndex = 0;
+		retArr->push_back(this->routes[nIndex]);
+		*cost += this->adjMatrix[routeIndex][nIndex];
+		return;
+	}
+
+	minimumCost(nIndex, visited, cost, retArr);
+}
+
+// Dynamic Programming TSP (non-poly time) Second Helper
+uint32_t RouteFinder::closest(uint32_t routeIndex, bool* visited, double* cost) {
+	double minimum = DOUBLE_MAX;
+	double nc = UINT32_MAX;
+	double kmin;
+	
+	for (uint32_t i = 0; i < this->numNodes; i++) {
+		if (this->adjMatrix[routeIndex][i] != -1 && !visited[i]) {
+			if (this->adjMatrix[routeIndex][i] + this->adjMatrix[i][routeIndex] < minimum) {
+				minimum = this->adjMatrix[i][0] + this->adjMatrix[routeIndex][i];
+				kmin = this->adjMatrix[routeIndex][i];
+				nc = i;
+			}
+		}
+	}
+
+	if (minimum != DOUBLE_MAX) {
+		*cost += kmin;
+	}
+	return nc;
+}
+
 // Algorithm: https://www.programiz.com/dsa/prim-algorithm
+// NOTE: Prims wont work for directed graphs, which is what we have
+// Switch to Edmond's Algorithm
 void RouteFinder::primsMST(double** result, bool* chosen) {
 	uint32_t numEdges = 0;
 	chosen[0] = true;
@@ -181,6 +255,8 @@ void RouteFinder::primsMST(double** result, bool* chosen) {
 		numEdges++;
 	}
 }
+
+
 
 void RouteFinder::getOddDegreeNodes(double** subEdges, bool* subNodes, bool* result) {
 	uint32_t count;
@@ -216,13 +292,14 @@ void RouteFinder::extractSubgraph(uint32_t* numEdges, uint32_t* numNodes, bool* 
 // This algorithm is too big brain for me to comprehend
 // Just gonna port it to use this library: https://github.com/dilsonpereira/Minimum-Cost-Perfect-Matching
 void RouteFinder::findMinWeightPerfectMatching(uint32_t* numNodes, uint32_t* numEdges, bool* subNodes, double** subGraph, double** result) {
-	Graph G(*numNodes);
+	Graph G(this->numNodes);
 	vector<double> cost(*numEdges);
 	for (uint32_t i = 0; i < this->numNodes; i++) {
 		if (subNodes[i]) {
 			for (uint32_t j = 0; j < this->numNodes; j++) {
 				if (subGraph[i][j] != -1) {
 					G.AddEdge(i, j);
+					uint64_t tmp = G.GetEdgeIndex(i, j);
 					cost[G.GetEdgeIndex(i, j)] = (int)(subGraph[i][j] * INT_DOUBLE_ACCURACY);		// This library processes ints, multiply for fixed accuracy increase
 				}
 			}
@@ -230,16 +307,28 @@ void RouteFinder::findMinWeightPerfectMatching(uint32_t* numNodes, uint32_t* num
 	}
 	
 	Matching M(G);
-	pair<list<int>, double> solution = M.SolveMinimumCostPerfectMatching(cost);
-	for (list<int>::iterator it = solution.first.begin(); it != solution.first.end(); it++) {
-		pair<int, int> e = G.GetEdge(*it);
-		result[e.first][e.second] = (double) G.GetEdgeIndex(e.first, e.second) / (double) INT_DOUBLE_ACCURACY;
-		result[e.second][e.first] = (double) G.GetEdgeIndex(e.first, e.second) / (double) INT_DOUBLE_ACCURACY;
+	try {
+		pair<list<int>, double> solution = M.SolveMinimumCostPerfectMatching(cost);
+		for (list<int>::iterator it = solution.first.begin(); it != solution.first.end(); it++) {
+			pair<int, int> e = G.GetEdge(*it);
+			result[e.first][e.second] = (double)G.GetEdgeIndex(e.first, e.second) / (double)INT_DOUBLE_ACCURACY;
+			result[e.second][e.first] = (double)G.GetEdgeIndex(e.first, e.second) / (double)INT_DOUBLE_ACCURACY;
+		}
 	}
+	catch (...) {
+		// Perfect matching not found, graph probably too small for matching
+		for (uint32_t i = 0; i < this->numNodes; i++) {
+			for (uint32_t j = 0; j < this->numNodes; j++) {
+				if (subGraph[i][j] != -1)
+					result[i][j] = subGraph[i][j];
+			}
+		}
+	}
+	
 }
 
 // Algorithm: https://cp-algorithms.com/graph/euler_path.html#algorithm
-// DEBUG: Destroys subgraph
+// DEBUG: Mutates subgraph
 void RouteFinder::findEulerTour(bool* subNodes, double** subGraph, vector<Route*>* result) {
 	bool* addedToTour = new bool[this->numNodes];
 	memset(addedToTour, false, sizeof(bool) * this->numNodes);
@@ -252,7 +341,7 @@ void RouteFinder::findEulerTour(bool* subNodes, double** subGraph, vector<Route*
 		degreeCount = 0;
 		currIndex = nodeStack.top();
 		for (uint32_t i = 0; i < this->numNodes; i++) {
-			if (subGraph[currIndex][i] != -1) {
+			if (subGraph[currIndex][i] != -1 && !addedToTour[i]) {
 				degreeCount++;
 
 				// Remove edge from subgraph
@@ -264,9 +353,10 @@ void RouteFinder::findEulerTour(bool* subNodes, double** subGraph, vector<Route*
 		
 		if (degreeCount == 0) {
 			if (!addedToTour[currIndex]) {
-				result->push_back(this->routes[currIndex]);
+				if (currIndex != 0) result->push_back(this->routes[currIndex]);
 				addedToTour[currIndex] = true;
 			}
+			nodeStack.pop();
 		}
 	}
 	
--- UAS-Pathfinding/RouteFinder.h
+++ UAS-Pathfinding/RouteFinder.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <vector>
+#include <string>
 #include "Route.h"
 #include "Edge.h"
+#include "Waypoint.h"
+#include "../submodules/Minimum-Cost-Perfect-Matching/Matching.h"
+#include "../submodules/Minimum-Cost-Perfect-Matching/Graph.h"
 
 class RouteFinder
 {
@@ -9,20 +13,27 @@ private:
 	std::vector<Route*> routes;
 	double** adjMatrix;
 	uint32_t numNodes;
+	double distToStartingRoute;
+	Waypoint* startingPoint;
+	double maxDist;
 
 public:
 	// Constructors
-	RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint);
+	RouteFinder(std::vector<Route*> routes, Waypoint* firstPoint, double maxDist);
 
 	// Destructors
 	~RouteFinder();
 
 	// Functions
-	std::vector<Route*> findShortestTraversal(double maxDist);
+	std::vector<Route*> findShortestTraversal();
 
 private:
+	// non-poly time
+	void minimumCost(uint32_t routeIndex, bool* visited, double* cost, std::vector<Route*>* retArr);
+	uint32_t closest(uint32_t routeIndex, bool* visited, double* cost);
 	// Traversal helper functions
 	void primsMST(double** result, bool* chosen);
+	void edmondsMST(double** result, bool* chosen);
 	void getOddDegreeNodes(double** subEdges, bool* subNodes, bool* result);
 	void extractSubgraph(uint32_t* numNodes, uint32_t* numEdges, bool* subNodes, double** result);
 	void findMinWeightPerfectMatching(uint32_t* numNodes, uint32_t* numEdges, bool* subNodes, double** subGraph, double** result);
