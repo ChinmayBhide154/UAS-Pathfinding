45c825ec32e30a18d4e5c7f9ea58da720b796278
Chinmay Bhide <Chinmay.Bhide154@gmail.com>
2022-11-26 11:20:40 -0800

    Setting Up Pattern Matching to parse JSON data

--- UAS-Pathfinding/main.cpp
+++ UAS-Pathfinding/main.cpp
@@ -2,19 +2,35 @@
 *  Created by: Justin Wong, Chinmay Bhide, Benjamin Brett
 */
 #include <iostream>
+#include <vector>
+#include <string.h>
 #include "JSON.h"
-
-
 #include <fstream>
-// #include <nlohmann/json.hpp>    // NOTE: Change to the line below
+#include <stdexcept>
+#include <map>
 #include "../submodules/json/single_include/nlohmann/json.hpp"
 using json = nlohmann::json;
 int main()
 {
     std::ifstream f("Text.json");
     json data = json::parse(f);
-    json otherdata = data.at("Waypoint0");
-    std::cout << otherdata.at("refLat");
-    std::cout << otherdata.at("refLong");
+
+    std::vector<json> waypoints = {};
+    std::string waypointNumber = "";
+    for (int i = 0; i < 4; i++) {
+        waypointNumber = std::to_string(i);
+        waypoints.push_back(data.at("Waypoint" + waypointNumber));
+        std::cout << "Waypoint" + waypointNumber << "\n";
+    }
+
+
+    
+
+    //json waypoint0 = data.at("Waypoint0");
+    //json refLat = waypoint0.at("refLat");
+    //json refLong = waypoint0.at("refLong");
+
+    //std::cout << refLat;
+
     return 0;
 }
